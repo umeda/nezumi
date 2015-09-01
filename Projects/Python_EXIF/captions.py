f49eac76d2f28c325cbc5de9b74991b5973f2129
umeda <umeda@example.com>
2015-08-31 23:51:58 -0700

    A few more changes.
    Signed-off-by: umeda <umeda@example.com>

--- Projects/Python_EXIF/captions.py
+++ Projects/Python_EXIF/captions.py
@@ -1,4 +1,5 @@
 from sys import argv
+import os
 
 if len(argv) < 2:
 	print 'oops - you forgot to enter a file name'
@@ -10,8 +11,13 @@ print filename
 
 with open(filename, 'rU') as fn:
   for line in fn:
-     print line
+     photo_and_caption = line.split('|')
+     print photo_and_caption[0]
+     print photo_and_caption[1]
 
 #http://www.sno.phy.queensu.ca/~phil/exiftool/faq.html
 #exiftool -title="test test test" bigben.jpg
 
+print os.path.basename(filename)
+path, fn = os.path.split(filename)
+print path
