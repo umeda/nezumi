f2b2c2e43b695d30b99a4f5aee69b8e4f4396c7f
umeda <umeda@users.noreply.github.com>
2024-07-05 21:14:22 -1000

    Update dc-vfo-03.ino

--- Projects/vco_for_tayloe/dc-vfo-03.ino
+++ Projects/vco_for_tayloe/dc-vfo-03.ino
@@ -12,13 +12,30 @@
     Based on code from Paul, VK3HN
     https://github.com/prt459/Arduino_si5351_VFO_Controller_Keyer
 
-
-
     dc-vfo-01 original plus font change
     dc-vfo-02 add second vfo
               add phase shift of 90 degrees (set value to 50 - not working yet...) 
     dc-vfo-03 Add spaces for powers of 10^3
 
+    Copyright 2024 Nezumi Workbench
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights 
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+    copies of the Software, and to permit persons to whom the Software is furnished
+    to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
 */
 
 const unsigned long int FREQ_DEFAULT = 7100000ULL;
